eb9eda8a6e848316bd9993d2dbba201d1aaf3990
Tao Chen <taochensyd@gmail.com>
2021-12-26 21:12:44 +1100

    get location infomation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
 function App() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log("Latitude is :" + position.coords.latitude + " " + "Longitude is :" + position.coords.longitude);
+
+    });
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position)
+    });
+  } else {
+    console.log("Not Available");
+  }
+
+  
+
   return (
     <div>app.js</div>
   );
